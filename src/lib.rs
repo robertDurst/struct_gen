9e49592dbd88084e01b31858b5fa2ca63f654ca1
robertdurst <rsdurst@colby.edu>
2018-07-26 22:11:48 -0700

    instead of just adding struct constructor, also generate struct code and take in default expressions

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,21 @@
 #[macro_export]
 macro_rules! struct_gen (
-    ($s:ident) => {
+    ($s:ident {$( $y: ident : $x: ty > $v: expr)*} ) => (
+        struct $s {
+            $(
+                $y: $x,
+            )*
+        }
+        
        impl $s {
             pub fn new() -> $s {
                 println!("Making a new struct of type: {}", stringify!($s));
-                $s {}
+                $s {
+                    $(
+                        $y: $v,
+                    )*
+                }
             }
        }
-    };
+    );
 );
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@
 extern crate struct_gen;
 
 fn main() {
-    struct Example {}
-
-    struct_gen!(Example);
+    struct_gen!(
+        Example {
+                data: String > "String".to_string()
+                height: i32 > 10
+        }
+    );
 
     impl Example {
         fn exists(&self) {
-            println!("I exist!");
+            println!("I exist! My height is: {}", self.height);
         }
     }
 
